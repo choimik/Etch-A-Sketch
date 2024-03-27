4dd6d2dcfc705b0da490504af7668ee7d0b64b26
Mikael <mchoi7309@gmail.com>
2024-03-27 15:09:37 -0400

    functions made and base html
    Created base html for settings and made the reset. resizing and inputVerification functions

--- script.js
+++ script.js
@@ -1,39 +1,80 @@
+let board;
+let row;
+let coloredPixel = [];
+let coloredPixelSize = 0;
 
-createBoard(50,50);
+widthInput = document.querySelector("#width");
+heightInput = document.querySelector("#height");
+
+createBoard(50, 50);
 
 let container = document.querySelector(".container");
 let mouseDown = false;
 
-container.addEventListener('mousedown', () => {
+container.addEventListener("mousedown", (event) => {
     mouseDown = true;
+    event.target.style.backgroundColor = "blue";
+    coloredPixel[coloredPixelSize] = event.target;
+    coloredPixelSize++;
 });
-container.addEventListener('mouseup', () => {
+
+container.addEventListener("mouseup", () => {
     mouseDown = false;
 });
 
 container.addEventListener("mouseover", (event) => {
     if(event.target !== container && mouseDown == true){
         event.target.style.backgroundColor = "blue";
+        coloredPixel[coloredPixelSize] = event.target;
+        coloredPixelSize++;
+    }
+
+});
+
+let resize = document.querySelector("#resize");
+resize.addEventListener("click", () => {
+    let input = validateInput();
+    if(input === true){
+        resizeBoard(+widthInput.value, +heightInput.value, row);
+    }
+    else if(input === 1){
+        alert("Please input values less than 100 and greater than 0");
+    }
+    else{
+        alert("Please input only number values");
     }
 });
-let widthInput = querySelector("#width");
-let heightInput = querySelector("#height");
 
-
-let resize = querySelector("#resize");
-resize.addEventListener("click", ()=>{
-    
+let clear = document.querySelector("#clear");
+clear.addEventListener("click", () =>{
+    clearBoard();
 });
 
-
+// let heightTrue;
+// heightInput.textContent.forEach((element) =>{
+//     if(charCodeAt(element) >= 48 && charCodeAt(element) <= 57){
+//         heightTrue = true;
+//     }
+//     else{
+//         alert("Input only numbers");
+//     }
+// });
+// widthInput.textContent.forEach((element) =>{
+//     if(charCodeAt(element) >= 48 && charCodeAt(element) <= 57 && heightTrue){
+//         resize(widthInput.textContent, heightInput.textContent);
+//     }
+//     else{
+//         alert("Input only numbers");
+//     }
+// });
 
 
 
 
 
 function createBoard(width, height){
-    let board = new Array(height).fill(0).map(()=> new Array(width).fill(0));
-    let row = [height];
+    board = new Array(height).fill(0).map(()=> new Array(width).fill(0));
+    row = [height];
     let container = document.querySelector(".container");
 
     for (let i = 0; i < height; i++){
@@ -58,9 +99,38 @@ function createBoard(width, height){
     }
 }
 
-function resizeBoard(width, height){
-    for (let i = 0; i < height; i++){
-        row[i].remove();
-    }
+function resizeBoard(width, height, row){
+    row.forEach(element => element.remove());
     createBoard(width, height);
+    for (let i = 0; i < coloredPixelSize; i++){
+        coloredPixel[i].style.backgroundColor = "blue";
+    }
+}
+
+function clearBoard(){
+    for (let i = 0; i < coloredPixelSize; i++){
+        coloredPixel[i].style.backgroundColor = "transparent";
+    }
+}
+
+function validateInput(){
+    widthInput = document.querySelector("#width");
+    heightInput = document.querySelector("#height");
+
+    for (char of heightInput.value){
+        if(char.charCodeAt(0) < 48 || char.charCodeAt(0) > 57){
+            return false;
+        }
+    }
+    for (char of widthInput.value){
+        if(char.charCodeAt(0) < 48 || char.charCodeAt(0) > 57){
+            return false;
+        }
+    }
+
+    if(widthInput.value > 100 || heightInput.value > 100 || widthInput.value <= 0 || heightInput.value <= 0){
+        return 1;
+    }
+
+    return true;
 }
